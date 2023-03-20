55d088fa710fc76c09c48b83f16def72035ab813
Gareth Grindeland <gt.grindeland@gmail.com>
2023-03-20 13:54:06 -0700

    Modify script to also subtract, multiply, and divide the user input numbers.

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,21 @@
 function add(number1, number2) {
   return number1 + number2;
 }
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+function divide(number1, number2) {
+  return number1 / number2;
+}
 
 // user interface logic
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-window.alert(add(number1, number2));
+window.alert("the addition of your two numbers equals " + add(number1, number2));
+window.alert("subtracting the second number from the first number equals " + subtract(number1, number2));
+window.alert("the multiplication of your two numbers equals " + multiply(number1, number2));
+window.alert("dividing your first number by your second number equals " + divide(number1, number2));
